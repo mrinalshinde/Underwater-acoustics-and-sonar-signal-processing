35aeb2303f8d6ab277bc3765d67ea604fb410ffd
Mrinal Shinde <mrinalgoa@gmail.com>
2018-04-22 23:00:20 +0200

    surface reverberation complete

--- USP3_2.m
+++ USP3_2.m
@@ -1,4 +1,34 @@
-
+frequency = 50; 
+grazingAngle = 0:0.01:pi/2;
+n = length(grazingAngle);
+theta = linspace(0,90,n);
+for i = 0:1:3
+    vW = 5*(2^i);
+    subplot(2,2,i+1);
+    for j=0:1:3
+frequency = 50*(2^j);
+beta = 4*((vW+2)/(vW+1)) + (2.5*((frequency+0.1)^(-1/3))-4) ...
+    .*cos(grazingAngle).^(1/8);
+sS = 10*log10(((tan(grazingAngle)).^beta) ...
+    .*((1+vW)^2)*((frequency+0.1)^(vW/150))*(10^(-5.05)));
+grid on; 
+axis([0 100 -70 10])
+plot(theta,sS,'LineWidth',2),
+title(['Wind speed = ',num2str(vW),'kn']),
+xlabel('Grazing angle [deg]'),
+ylabel('Surface reverberation [dB/m^2]') 
+ax = gca;
+ax.FontSize = 12;
+ax.YLim = [-70 10];
+%ax.Xlim = [0 90];
+hold on
+    end
+end
+hL = legend('f = 50 kHz','f = 100 kHz','f = 200 kHz','f = 400 kHz');
+newPosition = [0.85 0.85 0.2 0.2];
+newUnits = 'normalized';
+set(hL,'Position', newPosition,'Units', newUnits);
+hL.FontSize = 13;
 
 
 
--- trial1.m
+++ trial1.m
@@ -1,3 +1,43 @@
+clc;
+clear all;
+close all;
+f=[50000, 100000, 200000 ,400000] ;
+vw=[5, 10, 20, 40];
+%color = {'y','b','g','r','m'};
+theta= 0:0.01:pi/2;
+theta_axis = theta .* (180/pi);
+colors = ['r','g','b','m'];
+model_name = [{'At f=50kHz'} {'At f=100kHz'} {'At f=200kHz'} {'At f=400kHz'}];
+model_name2 = [{'At ws = 5 kn'} {'At ws = 10 kn'} {'At ws = 20 kn'} {'At ws = 40 kn'}];
+legend_names = [{'ws = 5 kn'},{'ws = 10 kn'},{'ws = 20 kn'},{'ws = 40 kn'}];
+legend_names2 = [{'f = 50 kHz'},{'f = 100 kHz'},{'f = 200 kHz'},{'f = 400 kHz'}];
+for p=1:2
+figure(p)
+for l=1:4
+subplot (2,2,l)
+for q=1:4
+if p==1
+bet=4*((vw(q)+2)/(vw(q)+1))+(2.5*((f(l)+0.1)^(-1/3))-4).*cos(theta).^(1/8);
+Ss = 10*log10(10^-5.05 * (1+vw(q))^2 * (f(l)+0.1)^(vw(q)/150).*(tan(theta)).^bet);
+plot(theta_axis,Ss,'color',colors(q),'linewidth',2);
+legend(legend_names,'location','southeastoutside');
+hold on
+title(model_name(l));
+else
+bet=4*((vw(l)+2)/(vw(l)+1))+(2.5*((f(q)+0.1)^(-1/3))-4).*cos(theta).^(1/8);
+Ss=10*log10(((tan(theta)).^bet).*((1+vw(l))^2)*((f(q)+0.1)^(vw(l)/150))*(10^(-5.05)));
+plot(theta_axis,Ss,'color',colors(q),'linewidth',2);
+legend(legend_names2,'location','southeastoutside');
+hold on
+title(model_name2(l));
+end
+axis([0 2*40 -70 30])
+set(gcf,'units','points','position',[10,10,1000,850]);
+xlabel('Grazing angle [in degrees]');
+ylabel('Surface reverberation [dB/m^2]');
+end
+end
+end
 
 
 
